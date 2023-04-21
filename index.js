4f7ad09814ff2513ff66b3672875dde675cf3cce
Felix Oyioka <felixoyioka@gmail.com>
2023-04-21 14:57:43 +0300

    Complete JavaScript events lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const right = left + 40; // the dodger is 40px wide
+  
+    if (right < 400) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
